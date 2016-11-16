18645b65235749b29170e01eb75c64771937eef2
yingxu.fu <fuyingxu@126.com>
2016-11-16 14:00:15 +0800

    '第一次提交'

--- src/components/GalleryByReactApp.js
+++ src/components/GalleryByReactApp.js
@@ -5,8 +5,10 @@ var React = require('react/addons');
 
 // CSS
 require('normalize.css');
-require('../styles/main.scss');
+require('../styles/main.css');
+// 获取图片相关的数据
 var imageDatas = require('../data/imageDatas.json');
+
 // 利用自执行函数， 将图片名信息转成图片URL路径信息
 imageDatas = (function genImageURL(imageDatasArr) {
     for (var i = 0, j = imageDatasArr.length; i < j; i++) {
@@ -20,10 +22,9 @@ imageDatas = (function genImageURL(imageDatasArr) {
     return imageDatasArr;
 })(imageDatas);
 
-
 var GalleryByReactApp = React.createClass({
   render: function() {
-    return (<section className="stage"><section className="img-sec"></section><nav className="controller-nav"></nav></section>);
+  return (<section className="stage" ref="stage"><section className="img-sec"></section><nav className="controller-nav"></nav></section>);
   }
 });
 React.render(<GalleryByReactApp />, document.getElementById('content')); // jshint ignore:line
